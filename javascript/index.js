0b6bdb0ba33d74b0d6230145f5c4786dcf3bf598
Terminator <rishusingh9369@gmail.com>
2022-08-15 22:35:57 +0530

    This is my first website code

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+rightnav=document.querySelector('.rightnav')
+navlist=document.querySelector('.navlist')
+
+
+burger.addEventListener('click', ()=>{
+
+    rightnav.classList.toggle('v-class-res')
+    navlist.classList.toggle('v-class-res')
+    navbar.classList.toggle('h-nav-res')
+
+})
